0db0d53b2bd0a34dc746c9014e35efb659d35004
amrisak64-spec <amrisak64@gmail.com>
2026-06-05 15:34:38 +0530

    Add files via upload
    Add school report system files"

---
+++ firebase-config.js
@@ -0,0 +1,17 @@
+// Firebase Configuration - Prabha Gyan Jyoti Public School
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBvi2Px_kwKzzDWM0p1uGb9YKtPrZoEebY",
+  authDomain: "prabhagyanJyoti.firebaseapp.com",
+  projectId: "prabhagyanJyoti",
+  storageBucket: "prabhagyanJyoti.firebasestorage.app",
+  messagingSenderId: "167332170512",
+  appId: "1:167332170512:web:92fe5e1653654adcc4b395"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
